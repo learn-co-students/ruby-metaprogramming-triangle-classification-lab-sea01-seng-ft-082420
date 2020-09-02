227a9ae5f70846765264e2b1a5b51c59963109ba
MullaN <admullan@gmail.com>
2020-09-02 14:57:56 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if self.impossible?([side_1, side_2, side_3])
+      raise TriangleError
+    elsif side_1 == side_2 && side_2 == side_3
+      return :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_3 == side_1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  def impossible?(side_array)
+    side_array.length.times do
+      side = side_array.shift
+      if side >= side_array[0] + side_array[1] || side <= 0
+        return true
+      end
+      side_array << side
+    end
+    return false
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
