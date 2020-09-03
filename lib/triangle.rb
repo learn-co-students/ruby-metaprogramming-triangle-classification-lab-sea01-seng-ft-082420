2d89f48c16060aecf865f88fee527f338f5323c9
Cooliomoded <cooliomoded@gmail.com>
2020-09-03 16:04:06 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,6 +24,22 @@ class Triangle
       end
   end
 
+  def angles
+    if self.triangle_inequality? && self.sides_greater_0?
+      angles_of_triangle = "The angles are:"
+    else
+      raise TriangleError
+    end
+      radians_opposite_x = Math.acos((self.y**2 + self.z**2 - self.x**2).to_f/(2*self.y*self.z)).round(2)
+      opposite_x = (radians_opposite_x * (180/Math::PI)).round(2)
+      radians_opposite_y = Math.acos((self.x**2 + self.z**2 - self.y**2).to_f/(2*self.x*self.z)).round(2)
+      opposite_y = (radians_opposite_y * (180/Math::PI)).round(2)
+      radians_opposite_z = Math.acos((self.x**2 + self.y**2 - self.z**2).to_f/(2*self.x*self.y)).round(2)
+      opposite_z = (radians_opposite_z * (180/Math::PI)).round(2)
+      puts "#{angles_of_triangle}"
+      puts "For the vertex opposite to the side x, the angle is #{opposite_x}, for y, the angle is #{opposite_y}, for z, the angle is #{opposite_z}."
+  end
+
   def triangle_inequality?
     self.x + self.y > self.z && self.y + self.z > self.x && self.z + self.x > self.y
   end
