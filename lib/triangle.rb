0c66c02ba099474cf945e9079d65c5fc96387447
Margarita Morozova <ritamorozova@yahoo.com>
2020-09-11 09:43:53 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_reader :ab, :bc, :ac
+  
+  def initialize(ab, bc, ac)
+    @ab = ab
+    @bc = bc
+    @ac = ac
+  end
+
+  def kind
+    check_triangle
+    if ab == bc && bc == ac && ac == ab
+      :equilateral
+    elsif ab == bc || bc == ac|| ac == ab
+      :isosceles
+    else 
+      :scalene
+    end
+     
+
+  end  
+
+  def check_triangle
+    legal_triangle = [(ab+bc>ac), (ab+ac>bc), (bc+ac>ab)]
+    [ab, bc, ac].each do |side|
+      legal_triangle << false if side<=0 
+        raise TriangleError if legal_triangle.include?(false)
+      end
+  end 
+
+
+  class TriangleError < StandardError
+    def message 
+      "This is not a real triangle"
+    end 
+  end
+
+
 end
